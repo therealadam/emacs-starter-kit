954df4b81398eeb8ca96b351e26e6a4aab5165ac
Adam Keys <adam@therealadam.com>
2010-02-04 06:36:35 -0600

    Better OS X integration and keybindings.

--- vendor/emacsd-tile.el
+++ vendor/emacsd-tile.el
@@ -14,19 +14,19 @@
         (set-window-start  this-window  other-start)
         (set-window-start  other-window this-start)))))
 
-(global-set-key (kbd "C-M-J") (lambda () (interactive) (swap-with 'down)))
-(global-set-key (kbd "C-M-K") (lambda () (interactive) (swap-with 'up)))
-(global-set-key (kbd "C-M-H") (lambda () (interactive) (swap-with 'left)))
+(global-set-key (kbd "C-M-K") (lambda () (interactive) (swap-with 'down)))
+(global-set-key (kbd "C-M-I") (lambda () (interactive) (swap-with 'up)))
+(global-set-key (kbd "C-M-J") (lambda () (interactive) (swap-with 'left)))
 (global-set-key (kbd "C-M-L") (lambda () (interactive) (swap-with 'right)))
 
-(global-set-key (kbd "M-J") (lambda () (interactive) (enlarge-window 1)))
-(global-set-key (kbd "M-K") (lambda () (interactive) (enlarge-window -1)))
-(global-set-key (kbd "M-H") (lambda () (interactive) (enlarge-window -1 t)))
+(global-set-key (kbd "M-K") (lambda () (interactive) (enlarge-window 1)))
+(global-set-key (kbd "M-I") (lambda () (interactive) (enlarge-window -1)))
+(global-set-key (kbd "M-J") (lambda () (interactive) (enlarge-window -1 t)))
 (global-set-key (kbd "M-L") (lambda () (interactive) (enlarge-window 1 t)))
 
-(global-set-key (kbd "M-j") 'windmove-down)
-(global-set-key (kbd "M-k") 'windmove-up)
-(global-set-key (kbd "M-h") 'windmove-left)
+(global-set-key (kbd "M-k") 'windmove-down)
+(global-set-key (kbd "M-i") 'windmove-up)
+(global-set-key (kbd "M-j") 'windmove-left)
 (global-set-key (kbd "M-l") 'windmove-right)
 
 (provide 'emacsd-tile)
